8bc1d23e3fcd6b99ac225163a8dadd5a464c0fee
Ajang Rahmat <41977490+ajangrahmat@users.noreply.github.com>
2022-08-29 19:02:55 +0700

    Tambah Coding Utama

---
+++ main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const win = new BrowserWindow();
+    win.loadFile('index.html');
+}
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
